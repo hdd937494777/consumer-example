17f350a147e5488c642537aea320cb5e350dbdc6
wangkang <wangkang@51kik.com>
2020-05-31 16:01:04 +0800

    增加Nacos配置

--- src/main/java/com/example/testing/controller/HelloController.java
+++ src/main/java/com/example/testing/controller/HelloController.java
@@ -1,18 +1,24 @@
 package com.example.testing.controller;
 
+import com.example.testing.service.HelloService;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
 
     @Reference
-    private com.example.testing.service.HelloService helloService;
+    private HelloService helloService;
+
+    @Value("${local.name}")
+    private String name;
 
     @GetMapping("/hello")
-    public String hello(@RequestParam String name) {
+    public String hello() {
         return helloService.hello(name);
     }
 }
