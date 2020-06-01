f72d757fdae7aa9268d580167ace1ce6a0655e72
wangkang <wangkang@51kik.com>
2020-06-01 16:30:21 +0800

    修复dubbo协议远程调用失败的问题

--- src/main/java/com/example/testing/controller/HelloController.java
+++ src/main/java/com/example/testing/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.example.testing.controller;
 
 import com.example.testing.service.HelloService;
 import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +13,8 @@ public class HelloController {
     @Reference
     private HelloService helloService;
 
-    @Value("${local.name}")
-    private String name;
-
     @GetMapping("/hello")
     public String hello() {
-        return helloService.hello(name);
+        return helloService.hello();
     }
 }
